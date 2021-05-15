adc0608ecc5c0f29fcfe7e31691259a237ed2e64
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-15 22:33:48 +0800

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+    
     return(
         <div className="home">
             <h2>Homepage</h2>
+            
+            {/* Never invoke function automatically, plain instead of ().
+                Use anonymous function to pass function with parameter  #7*/}
+
+            <button onClick={() => handleClickAgain('mario')}>Click Me</button>
         </div>
     );
 }
