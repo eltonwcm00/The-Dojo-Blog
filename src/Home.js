381deb74dfce20984bf183ca02173382376ae573
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-16 17:14:41 +0800

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -6,27 +6,36 @@ const Home = () => {
     /* Use Props (pass data from parents to its child) */
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     /* useEffect will run everytime re-render of the component */
     /* useEffect dependencies [] is to ensure this function() will be re-render only when a condition is met */
-    /* Fetch JSON data from the endpoint and  update the state*/
+    /* Fetch JSON data from the endpoint and update the state*/
     useEffect(() => {
        setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('Could not fetch the data for the result');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
        }, 1000);
     }, []);
-
 
     return(
         <div className="home">
           {/* Conditional templating (&&), blogs do existing, render only when it got the data */}
+          {error && <div>{error}</div>}
           {isPending && <div>Loading...</div>}
           {blogs && <BlogList blogs_passing={blogs} title_passing="All Blogs"/>}
         </div>
