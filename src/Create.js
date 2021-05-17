a0cb6a2ab9af8395c22ff060b4ba0362f18e5f7e
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-17 22:05:55 +0800

    making post request

--- src/Create.js
+++ src/Create.js
@@ -5,19 +5,30 @@ const Create = () => {
     const[title, setTitle] = useState('');
     const[body, setBody] = useState('');
     const[author, setAuthor] = useState('mario');
-
+    const[isPending, setIsPending] = useState(false);
+  
     const handleSubmit = (e) => {
-        /* Prevent the page to reload when submit button is clicked */
-        e.preventDefault();
-        /* Create JSON object to take in the input and save into db.json */
-        const blog = {title, body, author};
 
+            /* Prevent the page to reload when submit button is clicked */
+            e.preventDefault();
+            /* Create JSON object to take in the input and save into db.json */
+            const blog = {title, body, author};
+            setIsPending(true);
+            /* Making POST request */ 
+            fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+            }).then(() => {
+                console.log('New blog is added');
+                setIsPending(false);
+            })
     }
 
     return(
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form onSubmit={}>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 {/* Two way binding : onChange will then change the title when try to change the input field value
                     use (event) e.target.value to track the value */}
@@ -38,10 +49,8 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
     );
