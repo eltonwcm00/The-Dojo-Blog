238f526d0b7db68b36f1d09f5d4dbb2bc186c0c5
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-17 22:22:45 +0800

    delete request

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import useFetch from "./useFetch";
+import { useHistory } from 'react-router-dom';
 
 const BlogDetails = () => {
 
@@ -7,6 +8,15 @@ const BlogDetails = () => {
        destructure the id (/:id) */
     const {id} = useParams();
     const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
+    const redirectUser = useHistory();
+    
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            redirectUser.push('/');
+        });
+    }
 
     return(
         <div className="blog-details">
@@ -19,6 +29,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2><br/>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
