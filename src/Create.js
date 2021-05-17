1dd6d76c97128737a3691b1f8d53a73cedd45cbe
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-17 21:47:46 +0800

    submit event

--- src/Create.js
+++ src/Create.js
@@ -6,13 +6,21 @@ const Create = () => {
     const[body, setBody] = useState('');
     const[author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        /* Prevent the page to reload when submit button is clicked */
+        e.preventDefault();
+        /* Create JSON object to take in the input and save into db.json */
+        const blog = {title, body, author};
+
+    }
+
     return(
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={}>
                 <label>Blog title:</label>
                 {/* Two way binding : onChange will then change the title when try to change the input field value
-                    use e.target.value to track the value */}
+                    use (event) e.target.value to track the value */}
                 <input type="text" 
                     required 
                     value ={title}
